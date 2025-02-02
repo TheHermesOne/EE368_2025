c2e2b56370c74b79646b903824698633191b5aab
Hermes77 <isaaknathan@gmail.com>
2025-02-02 17:39:34 -0500

    html redirects and basic session work

--- Frontend.py
+++ Frontend.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
 
 app = Flask(__name__)
+app.secret_key='gnbgjnbvgjnvfynbvfyjnvfkmnvghkm;09654'
 @app.route('/')
 def index():
+    if 'userEmail' in session:
+        return redirect(url_for('welcomepage', UserEmail=session["userEmail"]))
     return redirect(url_for('login'))
-@app.route('/login', methods= ['POST'])
+@app.route('/login', methods= ['GET','POST'])
 def login():
     if request.method == 'POST':
         if 'login' in request.form:
-            username = request.form.get('uname')
-            password = request.form.get('psw')
-            app.logger.info(f"Username: {username}, Password: {password}")
+            session['userEmail'] = request.form.get('mail')
+            session['password'] = request.form.get('psw')
+            return redirect(url_for('welcomepage',UserEmail=session['userEmail']))
     return render_template('login.html')
 @app.route('/signup')
 def signup():
@@ -19,10 +22,15 @@ def signup():
 @app.route('/changepassword')
 def changepassword():
     return render_template('changepassword.html')
+@app.route('/welcomepage/<UserEmail>')
+def welcomepage(UserEmail = None):
+    return render_template('welcomepage.html',user=UserEmail)
 
-@app.route('/welcomepage')
-def welcome():
-    return render_template('welcomepage.html')
+@app.route('/logout')
+def logout():
+    session.pop('userEmail',None)
+    session.pop('password',None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
