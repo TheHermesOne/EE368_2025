94a3fe9a14588fd4df49461a5c589892aadfd3ac
Hermes77 <isaaknathan@gmail.com>
2025-02-02 17:06:10 -0500

    Html-flask Interaction succesful

--- Frontend.py
+++ Frontend.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 @app.route('/')
+def index():
+    return redirect(url_for('login'))
+@app.route('/login', methods= ['POST'])
 def login():
+    if request.method == 'POST':
+        if 'login' in request.form:
+            username = request.form.get('uname')
+            password = request.form.get('psw')
+            app.logger.info(f"Username: {username}, Password: {password}")
     return render_template('login.html')
-
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
