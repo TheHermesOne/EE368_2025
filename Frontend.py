a8ce413c8392203f62d87d016420de5189471a74
Hermes77 <isaaknathan@gmail.com>
2025-02-02 15:21:20 -0500

    basic flask layout

--- Frontend.py
+++ Frontend.py
@@ -2,10 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Flask is working!"
+    return "Login Page"
 
+@app.route('/SignUp')
+def home():
+    return "Sgin Up Page"
 
+@app.route('/ResetPassword')
+def home():
+    return "Reset Password Page"
 
+@app.route('/Success')
+def home():
+    return "Sucess Page"
 
 if __name__ == "__main__":
     app.run(debug=True)
