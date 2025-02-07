134f3b2b8118daaf66b45fc07498ac7430ea7502
Hermes77 <isaaknathan@gmail.com>
2025-02-07 10:57:55 -0500

    Finished data pass through

--- OauthApp/__init__.py
+++ OauthApp/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+
+mysqlUser = 'username'
+mysqlPass = 'qwe123qwe'
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+mysqlconnector://username:qwe123qwe@localhost/flask_db')
+app.secret_key = 'gnbgjnbvgjnvfynbvfyjnvfkmnvghkm;09654'
+app.config['SQLALCHEMY_DATABASE_URI'] = (f'mysql+mysqlconnector://{mysqlUser}:{mysqlPass}@localhost/flask_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
